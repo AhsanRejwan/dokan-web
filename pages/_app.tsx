ea26b2ed98b8adef86d8c4fba6408c01dbf73a36
AhsanRejwan <ahsan.zaman@ideascale.com>
2021-09-25 19:16:45 +0600

    Created app header.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 import {AlertContextProvider} from "../contexts/AlertContext";
 import React from "react";
+import {AppContextProvider} from "../contexts/AppContext";
 
 const MyApp = ({Component, pageProps}: AppProps) => {
     return (
         <AlertContextProvider>
+            <AppContextProvider>
                 <Component {...pageProps} />
+            </AppContextProvider>
         </AlertContextProvider>
     )
 }
